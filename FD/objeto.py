a0ee759130840499ae7147b239693e77288732ad
lorduy17 <pablolorduy2004@gmail.com>
2025-08-27 23:18:00 -0500

    Update objeto.py
    Interface now delegates body loading and normalization elsewhere

--- FD/objeto.py
+++ FD/objeto.py
@@ -6,22 +6,39 @@ from tkinter import filedialog
 class maya():
 
     def __init__(self):
+        self.model = None
+        self.original_vectors = None
+        self.normalized_vectors = None
 
-        self.ruta_stl = filedialog.askopenfilename(title="Selecciona archivo STL",filetypes=[("Archivos STL","*.stl")])
-        if not self.ruta_stl:
-            raise ValueError("No se seleccionó archivo STL")
+    def file_load(self, filename: str):
+            if not filename:
+                raise ValueError("STL file not found")
+            self.model = mesh.Mesh.from_file(filename)
+            self.original_vectors = self.model.vectors.copy()
+            self.normalized_vectors = None
 
-
-    def carga_archivo(self):
-
-        self.ruta_stl = filedialog.askopenfilename(
-        title="Selecciona un archivo STL",
-        filetypes=[("Archivos STL", "*.stl")]
-        )
-    def solido(self):
-        
-        modelo = mesh.Mesh.from_file(self.ruta_stl)
-        # np.savetxt("puntos_stl.txt", puntos, fmt="%.6f", delimiter="\t")
-        return modelo.vectors
+    def get_original_vertices(self):
+         if self.original_vectors is None:
+             raise ValueError("No STL file loaded")
+         return self.original_vectors
     
+    def get_normalized_vertices(self):
+        if self.original_vectors is None:
+            raise ValueError("No STL file loaded")
+        
+        if self.normalized_vectors is not None:
+            return self.normalized_vectors
+        
+        model_flat = self.model.vectors.reshape(-1,3)
 
+        centroid = np.mean(model_flat, axis=0)
+        model_centered = model_flat - centroid
+        
+        ranges = np.ptp(model_centered, axis=0)
+        max_range = np.max(ranges)
+        if max_range == 0:
+         max_range = 1
+
+        normalized = model_centered/ max_range
+        self.normalized_vectors = normalized.reshape(self.model.vectors.shape)
+        return self.normalized_vectors
