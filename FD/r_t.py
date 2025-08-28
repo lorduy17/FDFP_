4f9d3f5a2de98c935119c75ec5ed2ed345dbbc6d
lorduy17 <pablolorduy2004@gmail.com>
2025-08-28 16:25:18 -0500

    Update r_t.py
    Update operations module to provide current aircraft state and dynamically compute rotation variables

--- FD/r_t.py
+++ FD/r_t.py
@@ -2,31 +2,73 @@ import numpy as np
 
 
 class operations:
-    @staticmethod
-    def rot_x(phi):
+
+    def __init__(self):
+        self.phi = 0
+        self.theta = 0
+        self.psi = 0
+        self.alpha = 0
+        self.beta = 0
+        self.gamma = 0
+        self.wind = np.array([0,0,0])
+        
+    def rot_x(self,phi):
+        self.phi = phi
         c = np.cos(np.radians(phi))
         s = np.sin(np.radians(phi))
         return np.array([[1, 0, 0],
                          [0, c, -s],
                          [0, s, c]])
     
-    @staticmethod
-    def rot_y(theta):
+     
+    def rot_y(self,theta):
+        self.theta = theta
         c = np.cos(np.radians(theta))
         s = np.sin(np.radians(theta))
         return np.array([[c, 0, s],
                          [0, 1, 0],
                          [-s, 0, c]])
     
-    @staticmethod
-    def rot_z(psi):
+     
+    def rot_z(self,psi):
+        self.psi = psi
         c = np.cos(np.radians(psi))
         s = np.sin(np.radians(psi))
         return np.array([[c, -s, 0],
                          [s, c, 0],
                          [0, 0, 1]])
     
-    @staticmethod
+    def update_state(self,alpha,beta,gamma,wind):
+
+        # check units
+        self.alpha = alpha
+        self.beta = beta
+        self.gamma = gamma
+        self.wind = wind
+    
+    def state_actual(self,):
+
+        return {
+            "angles": {
+            "alpha": self.alpha, # Angle of attack [deg]
+            "beta": self.beta, # Sideslip angle [deg]
+            "gamma": self.gamma # climb angle [deg]
+            },
+            "velocities_body": self.wind, # [u, v, w] in body frame
+            
+            # "angular_rates": np.array([p, q, r]), # [p, q, r] roll, pitch, yaw r
+            
+            "attitude": np.array([self.phi, self.theta, self.psi]) # [φ, θ, ψ] Euler angles [ra
+            
+        }
+         
+    
+    
+
+
+
+    
+    @staticmethod 
     def mult_matrix(*matrices):
         result = np.eye(3)
         for matrix in matrices:
